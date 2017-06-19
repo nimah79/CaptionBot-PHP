6190b89d023c06c56b4a7f8dc9111628c61633a0
Nima HeydariNasab <nima.heydari79@yahoo.com>
2017-06-19 19:54:28 +0430

    Remove writing cookie to txt file

--- captionbot.php
+++ captionbot.php
@@ -15,40 +15,34 @@ function curl_get_contents($url) {
   return $response;
 }
 
-function initCaptionBot() {
-  $content = file_get_contents('https://www.captionbot.ai/api/init', false, stream_context_create(["http" => ["header" => "Content-Type: application/json\r\ncharset: utf-8\r\n"]]));
-  saveCookies($http_response_header);
-  return json_decode($content);
-}
-
-function saveCookies($http_response_header) {
+function getCookie($http_response_header) {
   foreach($http_response_header as $header) {
     if(substr($header, 0, 10) === 'Set-Cookie'){
-      if(preg_match('@Set-Cookie: (([^=]+)=[^;]+)@i', $header, $matches)) {
-        file_put_contents('captionbot_cookie.txt', $matches[1]);
+      if(preg_match('/Set-Cookie: (([^=]+)=[^;]+)/i', $header, $matches)) {
+        return $matches[1];
       }
     }
   }
 }
 
-function sendToCaptionBot($data) {
+function sendToCaptionBot($data, $cookie) {
   $ch = curl_init('https://www.captionbot.ai/api/message');
   curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
   curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-  curl_setopt($ch, CURLOPT_COOKIESESSION, true);
-  curl_setopt($ch, CURLOPT_COOKIEFILE, 'captionbot_cookie.txt');
-  curl_setopt($ch, CURLOPT_COOKIEJAR, 'captionbot_cookie.txt');
+  curl_setopt($ch, CURLOPT_COOKIE, $cookie);
   $response = curl_exec($ch);
   curl_close($ch);
   return $response;
 }
 
 function getImageCaption($url) {
-  $conversationId = initCaptionBot();
+  $content = file_get_contents('https://www.captionbot.ai/api/init', false, stream_context_create(["http" => ["header" => "Content-Type: application/json\r\ncharset: utf-8\r\n"]]));
+  $cookie = getCookie($http_response_header);
+  $conversationId = json_decode($content);
   $data = ['userMessage' => $url, 'conversationId' => $conversationId, 'waterMark' => ''];
-  sendToCaptionBot($data);
+  sendToCaptionBot($data, $cookie);
   return json_decode(json_decode(curl_get_contents('https://www.captionbot.ai/api/message?'.http_build_query($data)), true),true)['BotMessages'][1];
 }
 
